09db810b75aae9376758fe40a022039437f72cfd
Aidan <aidan.wittish@student.sl.on.ca>
2025-09-21 18:10:26 -0400

    Final version of lab2

--- Lab-2/src/App.jsx
+++ Lab-2/src/App.jsx
@@ -6,13 +6,14 @@ function App() {
   return (
     <>
     <h1>Resorts Lite</h1>
+    <hr className="header-line"/>
     <div className="main-container">
         <Card image="src\assets\images\1.jpg" location="Indonesia" name="Gili Air Hotel" rating="4.8 ★" price="$589/night"/>
         <Card image="src\assets\images\2.jpg" location="Seychelles" name="Hilton Resort" rating="4.2 ★" price="$629/night"/>
-        <Card image="src\assets\images\3.jpg" location="US Virgin islands" name="Goa Resort" rating="3.5 ★" price="$485/night"/>
+        <Card image="src\assets\images\3.jpg" location="US Virgin islands" name="Goa Resort" rating="3.5 ★" price="$485/night" badRating={true}/>
         <Card image="src\assets\images\4.jpg" location="Bahamas" name="Kuredu Resort" rating="4.1 ★" price="$729/night"/>
         <Card image="src\assets\images\5.jpg" location="Mauritis" name="Trou D'eau Douce" rating="4.9 ★" price="$877/night"/>
-        <Card image="src\assets\images\6.jpg" location="Bermuda" name="Staniel Cay Hotel" rating="3.2 ★" price="$365/night"/>
+        <Card image="src\assets\images\6.jpg" location="Bermuda" name="Staniel Cay Hotel" rating="3.2 ★" price="$365/night" badRating={true}/>
     </div>
     </>
   );
--- Lab-2/src/Components/Card.jsx
+++ Lab-2/src/Components/Card.jsx
@@ -2,10 +2,10 @@ export default function Card(props) {
     return (
         <div className="Card-Component">
             <img src={props.image} alt="" width="200px"/>
-            <h2>{props.location}</h2>
-            <p>{props.name}</p>
-            <p>{props.rating}</p>
-            <p>{props.price}</p>
+            <h2 className="Resort-Location">{props.location}</h2>
+            <p className="Resort-Name">{props.name}</p>
+            <p>{props.badRating ? <h3 className="Rating-Bad">{props.rating}</h3> : <h3 className="Rating-Good">{props.rating}</h3>}</p>
+            <p className="Resort-Price">{props.price}</p>
         </div>
     );
 }
